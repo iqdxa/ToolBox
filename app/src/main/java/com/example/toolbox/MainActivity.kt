cd046ea545a5ea7ceb675efc5c34a981b5bb2e74
iqdxa <1429316040@qq.com>
2023-01-31 00:31:23 +0800

    修改主Activity

--- app/src/main/java/com/example/toolbox/MainActivity.kt
+++ app/src/main/java/com/example/toolbox/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.toolbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonMusic: Button =findViewById(R.id.button_music)
+        buttonMusic.setOnClickListener{
+
+        }
     }
+
+
 }
+
